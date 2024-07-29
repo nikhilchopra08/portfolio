dc99735f77862867804ca2acd2430f529de8c3fc
Nikhil chopra <nikhil0080012005@gmail.com>
2024-07-29 20:38:24 +0530

    updated the skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -58,7 +58,7 @@ export function AnimatedBeamDemo() {
             <p>Skills</p>
           </Circle>
           <Circle ref={div6Ref} className="h-16 w-16">
-            <Icons.MongoDb />
+            <Icons.Prisma />
           </Circle>
         </div>
         <div className="flex flex-row items-center justify-between">
@@ -144,8 +144,8 @@ const Icons = {
 </svg>
   ),
   MongoDb: () => (
-    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
-<path fill="#4db6ac" d="M44.284,36.035L27.687,1.429c-0.855-1.783-3.337-1.93-4.397-0.26L4.886,30.177	c-0.397,0.625-0.385,1.426,0.029,2.04l9.271,13.738c0.575,0.852,1.634,1.237,2.623,0.953l25.942-7.458	C44.208,39.031,44.939,37.402,44.284,36.035z M40.618,37.279L18.8,43.388c-0.505,0.142-0.98-0.305-0.87-0.818l7.735-36.097	c0.139-0.65,1.023-0.755,1.311-0.157l14.265,29.621C41.489,36.481,41.194,37.118,40.618,37.279z"></path>
-</svg>
+<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-brand-mongodb"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3v19" /><path d="M18 11.227c0 3.273 -1.812 4.77 -6 9.273c-4.188 -4.503 -6 -6 -6 -9.273c0 -4.454 3.071 -6.927 6 -9.227c2.929 2.3 6 4.773 6 9.227z" /></svg>
   ),
 };
+
+
